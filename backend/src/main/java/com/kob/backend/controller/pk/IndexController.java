d300dfd7422de9e28cafb8e2baf23c61016e06f7
LittleRubidium <lrb503@icloud.com>
2022-10-26 23:43:40 +0800

    build project frame

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -1,14 +1,20 @@
 package com.kob.backend.controller.pk;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 @RequestMapping("/pk/")
 public class IndexController {
 
-    @RequestMapping("index")
-    public String index() {
-        return "pk/index.html";
+    @RequestMapping("getbotinfo/")
+    public Map<String,String> getBotInfo() {
+        Map<String,String> bot = new HashMap<>();
+        bot.put("name","apple");
+        bot.put("rating","1800");
+        return bot;
     }
 }
