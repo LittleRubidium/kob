e3bfc31b6936cc56d2897bc701b343454fd44fd3
LittleRubidium <lrb503@icloud.com>
2022-11-06 22:16:03 +0800

    realize bot execute system

---
+++ backendcloud/botrunningsystem/src/main/java/com/kob/botruningsystem/utils/Bot.java
@@ -0,0 +1,8 @@
+package com.kob.botruningsystem.utils;
+
+public class Bot implements com.kob.botruningsystem.utils.BotInterface{
+    @Override
+    public Integer nextMove(String input) {
+        return 0;
+    }
+}
